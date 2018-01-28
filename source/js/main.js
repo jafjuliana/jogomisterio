cc029c04bfc4e1a6d06691a126a4742d6043e238
Juliana Assalti <juliana.assalti@wunderman.com>
2018-01-28 18:48:54 -0200

    Finalização da lógica

--- source/js/main.js
+++ source/js/main.js
@@ -1,25 +1,23 @@
 $(function() {
 
-    var objArmas, armas, objCriminosos, criminosos, objLocais, locais, objMisterioId, misterioId, idUser;
+    var objArmas, armas, objCriminosos, criminosos, objLocais, locais;
     var arrayArmas = [];
     var arrayCriminosos = [];
     var arrayLocais = [];
     var arraySorteio = [];
     var userCriminoso, userLocal, userArma, resultado;
+    var objMisterioId, objParse, idUser;
     $(".options").hide();
     $(".result").hide();
 
     //Listagem de armas, criminosos e locais
-    $.when($.ajax("https://handson.eniwine.com.br/api/descubraoassassino/armas"), $.ajax("https://handson.eniwine.com.br/api/descubraoassassino/criminosos"),$.ajax("https://handson.eniwine.com.br/api/descubraoassassino/locais"), $.ajax("https://handson.eniwine.com.br/api/descubraoassassino/")).done(function(resultA, resultC, resultL, resultId) {
+    $.when($.ajax("https://handson.eniwine.com.br/api/descubraoassassino/armas"), $.ajax("https://handson.eniwine.com.br/api/descubraoassassino/criminosos"),$.ajax("https://handson.eniwine.com.br/api/descubraoassassino/locais")).done(function(resultA, resultC, resultL) {
         objArmas = resultA[0];
         armas = JSON.parse(objArmas);
         objCriminosos = resultC[0];
         criminosos = JSON.parse(objCriminosos);
         objLocais = resultL[0];
         locais = JSON.parse(objLocais);
-        objMisterioId = resultId[0];
-        misterioId = JSON.parse(objMisterioId);
-        idUser = misterioId.misterioId;
 
         for (var j = 0; j < criminosos.length; j++) {
             arrayCriminosos[j] = criminosos[j].Id;
@@ -41,46 +39,76 @@ $(function() {
         arraySorteio[2] = Math.floor((Math.random() * armas.length) + 1);
     });
 
+    //Inicio do jogo
     $(".play").on("click", function(e) {
         e.preventDefault();
         $(".options").show("slow");
     });
 
+    //Resultado do jogo
     $(".finish").on("click", function(e, data) {
         e.preventDefault();
         userCriminoso = $('.criminoso').val();
         userLocal = $('.local').val();
         userArma = $('.arma').val();
 
-        if(arraySorteio[0] != userCriminoso && arraySorteio[1] != userLocal && arraySorteio[2] != userArma){
-            resultado = "Todos estão incorretos";
-        }
-        else if(arraySorteio[0] == userCriminoso && arraySorteio[1] == userLocal && arraySorteio[2] == userArma){
-            resultado = "Todos corretos, você solucionou o caso!!";
-        }
-        else if(arraySorteio[0] == userCriminoso && arraySorteio[1] == userLocal && arraySorteio[2] != userArma){
-            resultado = "O criminoso e local estão corretos"
-        }
-        else if(arraySorteio[0] == userCriminoso && arraySorteio[2] == userArma && arraySorteio[1] != userLocal){
-            resultado = "O criminoso e arma estão corretos"
-        }
-        else if(arraySorteio[1] == userLocal && arraySorteio[2] == userArma && arraySorteio[0] != userCriminoso){
-            resultado = "O local e arma estão corretos"
-        }
-        else if(arraySorteio[0] == userCriminoso && (arraySorteio[1] != userLocal && arraySorteio[2] != userArma)){
-            resultado = "Somente o criminoso esta correto"
-        }
-        else if(arraySorteio[1] == userLocal && (arraySorteio[0] != userCriminoso && arraySorteio[2] != userArma)){
-            resultado = "Somente o local esta correto"
-        }
-        else{
-            resultado = "Somente a arma esta correta"
-        }
+        //Verificação das respostas
+        if((userCriminoso == "" || userLocal == "" || userArma == "") && (userLocal == "" || userCriminoso == "" || userArma == "") && (userCriminoso == "" || userArma == "" || userLocal == "") && (userLocal == "" || userArma == "" || userCriminoso == "") && (userArma == "" || userLocal == "" || userCriminoso == "") && (userArma == "" || userCriminoso == "" || userLocal == "")){
+            alert("Digite todos os campos corretamente!");
+        }else{
+            if(arraySorteio[0] != userCriminoso && arraySorteio[1] != userLocal && arraySorteio[2] != userArma){
+                resultado = "Todos estão incorretos";
+            }
+            else if(arraySorteio[0] == userCriminoso && arraySorteio[1] == userLocal && arraySorteio[2] == userArma){
+                resultado = "Todos corretos, você solucionou o caso!!";
+            }
+            else if(arraySorteio[0] == userCriminoso && arraySorteio[1] == userLocal && arraySorteio[2] != userArma){
+                resultado = "O criminoso e local estão corretos"
+            }
+            else if(arraySorteio[0] == userCriminoso && arraySorteio[2] == userArma && arraySorteio[1] != userLocal){
+                resultado = "O criminoso e arma estão corretos"
+            }
+            else if(arraySorteio[1] == userLocal && arraySorteio[2] == userArma && arraySorteio[0] != userCriminoso){
+                resultado = "O local e arma estão corretos"
+            }
+            else if(arraySorteio[0] == userCriminoso && (arraySorteio[1] != userLocal && arraySorteio[2] != userArma)){
+                resultado = "Somente o criminoso esta correto"
+            }
+            else if(arraySorteio[1] == userLocal && (arraySorteio[0] != userCriminoso && arraySorteio[2] != userArma)){
+                resultado = "Somente o local esta correto"
+            }
+            else{
+                resultado = "Somente a arma esta correta"
+            }
 
-        $('.text-result').text(resultado);
-        $(".result").show("slow");
+            //Resultado na tela
+            $('.text-result').text(resultado);
+            $(".result").show("slow");
+
+            //Envio da API
+            $.ajax({type: 'GET', url: "https://handson.eniwine.com.br/api/descubraoassassino/", async: false, success: function(resultId){
+                objMisterioId = resultId;
+                objParse = jQuery.parseJSON(objMisterioId);
+                idUser = objParse.misterioId;
+            }});
+
+            $.ajax({
+                type: 'POST',
+                url: "https://handson.eniwine.com.br/api/descubraoassassino/",
+                data: {
+                    IdSuspeito: "userCriminoso",
+                    IdArma: "userArma",
+                    IdLocal: "userLocal",
+                    IdMisterio: "idUser",
+                },
+                success: function(resultData) { console.log("Enviado") }
+            });
+
+            //console.log(idUser);
+        }
     });
 
-    console.log(arraySorteio);
+    //console.log(arraySorteio);
+    
 
 });
